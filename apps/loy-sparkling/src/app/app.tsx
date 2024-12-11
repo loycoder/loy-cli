a5113848774bad0eafb5ca4ab67c9c3f22e2489d
liangyong <liangyong9098@cvte.com>
2024-12-11 11:40:25 +0800

    init: 调整项目结构

---
+++ apps/loy-sparkling/src/app/app.tsx
@@ -0,0 +1,13 @@
+// Uncomment this line to use CSS modules
+// import styles from './app.module.less';
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <div>
+      <NxWelcome title="loy-sparkling" />
+    </div>
+  );
+}
+
+export default App;
